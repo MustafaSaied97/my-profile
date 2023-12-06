23934fca658d2456bbcafdd935c69218101630bc
mustafasaied97 <mustafasaied97@gmail.com>
2023-12-06 02:43:47 +0200

    setup layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import 'src/assets/design/tailwind.css'
 function App() {
   return (
     <>
-    <Header/>
-    <Footer/>
+    <Header className=""/>
+    <div className="min-h-[--body-height] bg-[#7e6464] app-container">
+        body
+    </div>
+    <Footer className=""/>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <h1 className="btn-blue app-container bg-slate-500 text-3xl font-bold underline">
+    <h1 className="min-h-[--header-height] bg-slate-500 font-[Simplemade]    font-bold underline">
       Hello world!
     </h1>
   );
